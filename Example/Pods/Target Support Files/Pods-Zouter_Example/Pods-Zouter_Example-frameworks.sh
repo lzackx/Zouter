fa02ef3570bad51838f1c6e09c8f3cfa4bac7cf8
lzackx <lzackx@lzackx.com>
2021-05-27 14:12:00 +0800

    remove: Podfile dependency

--- Example/Pods/Target Support Files/Pods-Zouter_Example/Pods-Zouter_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-Zouter_Example/Pods-Zouter_Example-frameworks.sh
@@ -175,11 +175,9 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/ZKit/ZKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Zouter/Zouter.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/ZKit/ZKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Zouter/Zouter.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- Example/Pods/Target Support Files/ZKit/ZKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ZKit : NSObject
-@end
-@implementation PodsDummy_ZKit
-@end
